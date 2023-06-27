9f202ea8947e60876ba2e20ab7c2485683425ddf
Drbingbing <tw718140@gmail.com>
2023-06-27 14:55:15 +0800

    Initial Commit

---
+++ QuickDemoTemplate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickDemoTemplate
+//
+//  Created by 鍾秉辰 on 2023/6/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
